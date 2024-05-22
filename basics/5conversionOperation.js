6ef4f376aae65b29495d34725cba25f789071e3c
Armaan singh <130390550+armaan2002singh@users.noreply.github.com>
2024-05-22 14:30:30 +0000

    conversion & operators

---
+++ basics/5conversionOperation.js
@@ -0,0 +1,44 @@
+// OPERATIONS FOR CONVERSION
+
+let value = 3
+let negvalue = -value;
+
+// console.log(negvalue);
+
+// //   ARITHMETIC OPERATION
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1= "Armaan"
+let str2=" Singh"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2); // 12
+console.log(1+"2"); //12
+console.log("1"+"2"); //12
+console.log("1"+2+2); //122
+console.log(2+2+"1"); //41 reason is priority
+// ToPrimitive tc39 dcoumentry.
+//operator precedence
+// console.log(3+4*5%3);
+// console.log((3*4)+4);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); error
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1);
+
+let gamecounter = 100
+//++gamecounter; //first change then print
+gamecounter++; // change after usage
+console.log(gamecounter); // can study it for MDM.
